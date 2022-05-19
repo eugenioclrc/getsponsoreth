6c37f43589936f7abd462bef1959841120e91e92
eugenioclrc <eugenioclrc@gmail.com>
2022-05-19 12:37:43 -0300

    simple idee

--- hardhat/contracts/GetSponsorETH.sol
+++ hardhat/contracts/GetSponsorETH.sol
@@ -4,16 +4,27 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract GetSponsorETH {
+
+    mapping(uint256 => address) public ownerOf;
+    uint256 _counter = 1;
+
+    event NewSponsor(uint256 indexed, address indexed owner, string pledge);
+    event Fund(uint256 indexed, address indexed from, string author, string message);
+
     
     constructor() {
-        
     }
 
-    function createSponsor() external {
-        // todo
+    function createSponsor(string calldata pledge) external {
+        ownerOf[_counter] = msg.sender;
+        emit NewSponsor(_counter, msg.sender, pledge);
+        unchecked { _counter++; }
     }
 
-    function fund() external {
-        // todo
+    function fund(uint sponsorId, string calldata user, string calldata message) external payable {
+        address _owner = ownerOf[sponsorId];
+        require(_owner != address(0), "Sponsor not found");
+        payable(_owner).transfer(msg.value);
+        emit Fund(sponsorId, msg.sender, user, message);
     }
 }
--- hardhat/test/sample-test.js
+++ hardhat/test/sample-test.js
@@ -3,11 +3,21 @@ const { ethers } = require("hardhat");
 
 describe("WIP", function () {
   it("Should test something", async function () {
+    const [deployer, bob, alice] = await hre.ethers.getSigners();
+
+    
     const GetSponsorETH = await ethers.getContractFactory("GetSponsorETH");
     const getSponsorETH = await GetSponsorETH.deploy();
-    await greeter.getSponsorETH();
+    await getSponsorETH.deployed();
+    
 
+    expect(await getSponsorETH.ownerOf(1)).to.eq(ethers.constants.AddressZero);
     // create a sponsor
+    const tx = await getSponsorETH.createSponsor("Create a loans protocol");
+    await tx.wait()
+
+    expect(await getSponsorETH.ownerOf(1)).to.eq(deployer.address);
+
     // fund it
 
   });
