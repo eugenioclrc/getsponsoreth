865262152e1b59f552d9add3def062bb4c59f81e
eugenioclrc <eugenioclrc@gmail.com>
2022-05-20 10:54:26 -0300

    web initial commit

---
+++ web/src/lib/svgCheck.svelte
@@ -0,0 +1,5 @@
+<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24" class="inline-block h-6 w-6 stroke-current">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                  </svg>
---
+++ web/src/routes/index.svelte
@@ -0,0 +1,193 @@
+<script>
+import SvgCheck from "$lib/svgCheck.svelte";
+
+</script>
+
+    <div class="">
+        <div
+          class="from-primary to-secondary text-primary-content -mt-[4rem] grid place-items-center items-end bg-gradient-to-br pt-20">
+          <div
+            class="hero-content col-start-1 row-start-1 w-full max-w-7xl flex-col justify-between gap-10 pb-20 lg:flex-row lg:items-end lg:gap-0 xl:gap-20">
+            <div class="lg:px-10 lg:pb-12">
+              <h1 class="font-title mb-2 py-4 font-extrabold lg:py-10">
+                <div class="mb-2 text-5xl lg:text-7xl">Get Sponsort.Eth</div>
+                <div class="text-2xl lg:text-3xl">Get funds to buidl whatever you want<br />
+                 <!-- somethnig more here??-->
+                </div>
+              </h1>
+              <div class="flex w-full flex-col items-start space-y-10 lg:flex-row lg:space-x-4 lg:space-y-0">
+                <div class="my-2 flex max-w-sm flex-col gap-2 text-left">
+                  <div class="flex gap-2">
+                    <SvgCheck />
+                    Lorem ipsum factum
+                  </div>
+                  <div class="flex gap-2">
+                    <SvgCheck />
+                    Lorem ipsum factum
+                  </div>
+                  <div class="flex gap-2">
+                    <SvgCheck />
+                    Lorem ipsum factum
+                  </div>
+                  <div class="flex gap-2">
+                    <SvgCheck />
+                    Lorem ipsum factum
+                  </div>
+                  
+                </div>
+                
+              </div>
+              <div class="mt-4 flex flex-1 justify-center space-x-2 sm:justify-start lg:mt-6 lg:justify-start">
+                <a href="/action1" class="btn btn-ghost btn-active lg:btn-lg normal-case"><span
+                    class="hidden sm:inline">Action1</span>
+                  <span class="inline sm:hidden">act1</span>
+                </a>
+                <a href="/action2" class="btn btn-ghost btn-active lg:btn-lg normal-case"><span
+                    class="hidden sm:inline">Action2</span>
+                  <span class="inline sm:hidden">Acuerdo privado</span></a>
+                <a href="#how" class="btn lg:btn-lg normal-case">How it works?</a></div>
+            </div>
+          </div>
+          <svg class="fill-secondary col-start-1 row-start-1 h-auto w-full" width="1600" height="595"
+            viewBox="0 0 1600 595" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M0 338L53.3 349.2C106.7 360.3 213.3 382.7 320 393.8C426.7 405 533.3 405 640 359.3C746.7 313.7 853.3 222.3 960 189.2C1066.7 156 1173.3 181 1280 159.2C1386.7 137.3 1493.3 68.7 1546.7 34.3L1600 0V595H1546.7C1493.3 595 1386.7 595 1280 595C1173.3 595 1066.7 595 960 595C853.3 595 746.7 595 640 595C533.3 595 426.7 595 320 595C213.3 595 106.7 595 53.3 595H0V338Z">
+            </path>
+          </svg>
+        </div>
+       
+        <div class="hero bg-base-100 text-base-content mx-auto min-h-screen max-w-md md:max-w-full">
+          <div class="hero-content px-4 text-center md:px-0">
+            <div id="how">
+              <h2 class="mt-32 mb-2 text-4xl font-extrabold md:text-6xl">How it works?</h2>
+              <h3 class="mb-5 text-3xl font-bold">simple lorem ipsum</h3>
+              <p class="mx-auto mb-5 w-full max-w-lg">lorem ipsum factum won hackaton<br />
+                lorem ipsum sellspeach lorem iosum factum
+                </p>
+                
+              
+              <p></p>
+              <div class="mt-10 mb-20 flex flex-col lg:flex-row">
+                <div class="flex w-full flex-col text-left">
+                  <div class=" mx-auto w-full max-w-xs flex-grow sm:max-w-md">
+                    <b>Create a cause</b>, for example lorem ipsum loprem ipsum lorem ipsuem.<br />
+                <ul class="steps steps-vertical">
+    <li class="step">Crear cause</li>
+    <li class="step">Update your main page in markdown, upload to IPFS and pin it with to Storj</li>
+    <li class="step">Share with your followers</li>
+    <li class="step">Get funded</li>
+  </ul>    
+                  </div>
+                  <div class="mt-4 flex h-16 items-start justify-center">
+                    <a class="inline-block cursor-pointer rounded-md bg-indigo-600 px-4 py-3 text-center text-sm
+                     font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-indigo-700" href="#public">What is a cause?</a>
+                  </div>
+                </div>
+                
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        
+        <div class="hero from-primary to-accent text-primary-content min-h-screen bg-gradient-to-br" id="private">
+          <div class="hero-content mx-auto max-w-md text-center md:max-w-full">
+            <div>
+              <h2 class="mt-20 mb-2 text-4xl font-extrabold md:text-6xl">What its a cause (sponsor page?)</h2>
+              <h3 class="mb-5 text-3xl font-bold">Lorem ipsum</h3>
+              <p class="mx-auto mb-5 w-full max-w-lg">Lorem ipsum factum Todo! Lorem ipsum factum Todo Lorem ipsum factum Todo Lorem ipsum factum Todo 
+                 Lorem ipsum factum Todo Lorem ipsum factum Todo Lorem ipsum factum Todo Lorem ipsum factum Todo Lorem ipsum factum Todo Lorem ipsum factum Todo</p>
+            
+              <div class="my-20 flex flex-col">
+                <a class="btn btn-primary rounded-full" href="#todo">Create a cause (sponsor page?)</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--
+        <div class="hero bg-base-200 text-base-content min-h-screen py-10">
+          <div class="hero-content mx-auto w-full max-w-md text-center md:max-w-full">
+            <div class="w-full">
+              <h2 class="mt-20 mb-2 text-4xl font-extrabold md:text-6xl">Try it online</h2>
+              <p class="mx-auto mb-5 w-full md:max-w-lg">Use daisyUI file from CDN</p>
+              <div class="mx-auto my-2 w-full max-w-sm md:max-w-md lg:max-w-2xl">
+                <div
+                  class="mockup-code bg-neutral-focus mx-auto w-full max-w-xs text-left text-sm shadow-lg sm:max-w-none">
+                  <pre><code>
+    <span class="text-neutral-content text-opacity-40">&lt;link href=&quot;</span>https://cdn.jsdelivr.net/npm/daisyui@2.13.6/dist/full.css<span class="text-neutral-content text-opacity-40">&quot; rel=&quot;stylesheet&quot; type=&quot;text/css&quot; /&gt;</span>
+    <span class="text-neutral-content text-opacity-40">&lt;script src=&quot;</span>https://cdn.tailwindcss.com<span class="text-neutral-content text-opacity-40">&quot;&gt;&lt;/script&gt;</span>
+  
+  </code></pre>
+                </div>
+              </div>
+              <p class="mx-auto mt-10 mb-5 max-w-lg">Or try it online:</p>
+              <div class="flex flex-wrap justify-center gap-2"><a target="_blank" rel="noopener" href="/codepen"
+                  class="btn-outline btn gap-3 normal-case"><svg class="h-4 w-4 stroke-current" viewBox="0 0 64 64"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="2.3" stroke-linecap="round"
+                    stroke-linejoin="round">
+                    <path
+                      d="M3.06 41.732L32 60.932l28.94-19.2V22.268L32 3.068l-28.94 19.2zm57.878 0L32 22.268 3.06 41.732m0-19.463L32 41.47l28.94-19.2M32 3.068v19.2m0 19.463v19.2"
+                      stroke-width="6.05"></path>
+                  </svg>
+                  CodePen
+                </a>
+                <a href="/tailwindplay" class="btn-outline btn gap-3 normal-case" target="_blank" rel="noopener"><svg
+                    class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 54 33">
+                    <g clip-path="url(#prefix__clip0)">
+                      <path fill="#38bdf8" fill-rule="evenodd"
+                        d="M27 0c-7.2 0-11.7 3.6-13.5 10.8 2.7-3.6 5.85-4.95 9.45-4.05 2.054.513 3.522 2.004 5.147 3.653C30.744 13.09 33.808 16.2 40.5 16.2c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C36.756 3.11 33.692 0 27 0zM13.5 16.2C6.3 16.2 1.8 19.8 0 27c2.7-3.6 5.85-4.95 9.45-4.05 2.054.514 3.522 2.004 5.147 3.653C17.244 29.29 20.308 32.4 27 32.4c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C23.256 19.31 20.192 16.2 13.5 16.2z"
+                        clip-rule="evenodd"></path>
+                    </g>
+                    <defs>
+                      <clipPath id="prefix__clip0">
+                        <path fill="#fff" d="M0 0h54v32.4H0z"></path>
+                      </clipPath>
+                    </defs>
+                  </svg>
+                  Tailwind Play
+                </a>
+                <a sveltekit:prefetch href="/docs/install/" class="btn-outline btn gap-3 normal-case">
+                  <div class="flex"><img class="w-5" src="/logos/react.svg" alt="React" loading="lazy">
+                    <img class="w-5" src="/logos/vue.svg" alt="Vue" loading="lazy">
+                    <img class="w-5" src="/logos/svelte.svg" alt="Svelte" loading="lazy"></div>
+                  More examples
+                  <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                    <path
+                      d="M 18.71875 6.78125 L 17.28125 8.21875 L 24.0625 15 L 4 15 L 4 17 L 24.0625 17 L 17.28125 23.78125 L 18.71875 25.21875 L 27.21875 16.71875 L 27.90625 16 L 27.21875 15.28125 Z">
+                    </path>
+                  </svg>
+                </a></div>
+            </div>
+          </div>
+        </div>
+        <div class="hero bg-base-100 text-base-content mx-auto min-h-screen max-w-md md:max-w-full">
+          <div class="hero-content text-center">
+            <div class="w-full max-w-md">
+              <h2 class="mt-20 mb-2 text-4xl font-extrabold md:text-6xl">Install daisyUI</h2>
+              <p class="mx-auto mt-5 w-full max-w-lg">Pure CSS. Works on all frameworks. daisyUI can be used as a Tailwind
+                CSS plugin or as an independent CSS library</p>
+              <p class="mx-auto mt-5 w-full max-w-lg text-left">1. Install daisyUI as a Node package:</p>
+              <div class="mx-auto my-2 w-full max-w-md">
+                <div class="mockup-code mx-auto w-full max-w-xs text-left shadow-lg sm:max-w-none">
+                  <pre data-prefix="$"><code>npm i daisyui</code></pre>
+                </div>
+                <p class="mx-auto mt-5 w-full max-w-lg text-left">2. Add daisyUI to Tailwind CSS as a plugin:</p>
+                <div class="my-2 w-full max-w-4xl">
+                  <div class="mockup-code mx-auto w-full max-w-xs text-left text-sm shadow-lg sm:max-w-none">
+                    <pre><code><span class="text-neutral-content text-opacity-40">// tailwind.config.js</span>
+    </code></pre>
+                  </div>
+                </div>
+                <a href="/docs/install" class="btn-primary btn btn-wide mt-4 mb-20 shadow-lg">Install guide</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        -->
+      </div>
+  
+  <style>
+    .step {
+      text-align: left;
+    }
+  </style>
