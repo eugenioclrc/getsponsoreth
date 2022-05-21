932b2423a55d5c696fd6c61b09d7d5c9076e55aa
Deivitto <info.dcm62@gmail.com>
2022-05-21 21:56:53 +0200

    ui ux changes

--- web/src/routes/create-sponsor.svelte
+++ web/src/routes/create-sponsor.svelte
@@ -1,10 +1,11 @@
 <script context="module">
-	export const prerender = true;
+  export const prerender = true;
 </script>
-<script>
-  import { onMount } from 'svelte';
 
-  import {  contracts } from 'svelte-ethers-store';
+<script>
+  import { onMount } from "svelte";
+
+  import { contracts } from "svelte-ethers-store";
 
   let loading = false;
 
@@ -25,7 +26,7 @@
   $: backgroundImage = backgroundImages[randomIndex];
 
   async function onSubmit(e) {
-    if(loading) {
+    if (loading) {
       return;
     }
     loading = true;
@@ -37,29 +38,28 @@
       data[key] = value;
     }
 
-    const rawResponse = await fetch('/pindata', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      content: data.pledgeContent,
-      filetype: 'text/plain',
-      filename: 'content.md'
-    })
-  });
-  const content = await rawResponse.json();
-  console.log(content)
+    const rawResponse = await fetch("/pindata", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: data.pledgeContent,
+        filetype: "text/plain",
+        filename: "content.md",
+      }),
+    });
+    const content = await rawResponse.json();
+    console.log(content);
 
-  console.log(content);
+    console.log(content);
     console.log(data);
     // 1 upload file data and get the ipfs hash
     // 2 submit data using ethers
     // const tx = await $contracts.GetSponsortETH.create(data.username, $signerAddress, data.signature);
     loading = false;
   }
-
 </script>
 
 <link
@@ -72,7 +72,9 @@
   style="background-image: url({backgroundImage});"
 >
   <div class="hero-content flex-col lg:flex-row-reverse">
-    <div class="text-center lg:text-left lg:ml-8 new-proposal-container">
+    <div
+      class="text-center lg:text-left lg:ml-8 new-proposal-container max-w-sm"
+    >
       <h1 class="text-5xl font-bold shadow-text ">New proposal</h1>
       <p class="py-6  shadow-text">
         Create your own pledge proposal and get your own sponsor!
@@ -84,59 +86,76 @@
           <a href="/"><img alt="sponsor.eth" /></a>
         </div>
         <form on:submit|preventDefault={onSubmit}>
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">Name</span>
-          </label>
-          <input
-            type="text"
-            name="pledgeName"
-            placeholder="Let everybody know you"
-            class="input input-bordered"
-          />
-        </div>
-        <div class="flex flex-row align-baseline" style="    align-items: end;">
-          <div class="form-control ">
+          <div class="form-control">
             <label class="label">
-              <span class="label-text">Time in hours</span>
+              <span class="label-text">Name</span>
             </label>
             <input
-              type="number"
-              name="pledgeDuration"
-              placeholder="Duration of the pledge"
+              type="text"
+              name="pledgeName"
+              placeholder="Tell your sponsors who are you"
               class="input input-bordered"
             />
           </div>
-          <!-- checkbox for persisting over time, text right to the checkbox-->
-          <div class="form-control w-2/3 " style="align-items: end;">
-            <label class="label justify-start">
-              <input type="checkbox" name="pledgeForever" class="input input-bordered mr-1" />
-              <span class="label-text">Persist over time</span>
-            </label>
+          <div class="flex flex-row align-baseline" style="   ">
+            <div class="form-control ">
+              <label class="label">
+                <span class="label-text">Duration of the pledge</span>
+              </label>
+              <input
+                type="number"
+                name="pledgeDuration"
+                placeholder="Time in hours"
+                class="input input-bordered"
+                min="0"
+              />
+            </div>
           </div>
-        </div>
 
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">Pledge</span>
-          </label>
-          <textarea
-            name="pledgeContent"
-            placeholder="Tell why you want to be a sponsored, you can use Markdown"
-            class="h-24 input input-bordered pt-2"
-          />
-          <!-- <label class="label">
+          <!-- checkbox for persisting over time, text right to the checkbox-->
+          <div class="form-control  flex flex-row items-center" style="">
+            <!-- <label class="label justify-start">
+                          <input
+                            type="checkbox"
+                            name="pledgeForever"
+                            class="input input-bordered mr-1"
+                          />
+                          <span class="label-text">Persist over time</span>
+                        </label> -->
+            <label
+              class="material-checkbox-container label  "
+              style="    min-width: 30px;"
+            >
+              <input type="checkbox" name="pledgeForever" />
+              <span class="material-checkbox" />
+            </label>
+            <span class="label-text ml-1"
+              >The sponsoring period never expires</span
+            >
+          </div>
+
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Pledge</span>
+            </label>
+            <textarea
+              name="pledgeContent"
+              placeholder="Tell why you want to be sponsored. {'\n'}Note: You can use Markdown"
+              class="h-24 input input-bordered pt-2"
+            />
+            <!-- <label class="label">
             <p href="#" class="label-text-alt">
               .md is available for formatting
             </p>
           </label> -->
-        </div>
+          </div>
 
-        <div class="form-control mt-6">
-          <button class="button-charming btn" class:loading={loading}>Create pledge</button>
-        </div>
-      </form>
-
+          <div class="form-control mt-6">
+            <button class="button-charming btn" class:loading
+              >Create pledge</button
+            >
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -156,6 +175,7 @@
     border-radius: 10px;
     border: 1px solid rgba(255, 255, 255, 0.18);
     padding: 2rem;
+    max-width: none; /* remove max-width on 1000px or more TODO*/
   }
 
   .button-charming {
@@ -171,5 +191,35 @@
 
   .button-charming:hover {
     background-position: 185% 50%;
+  }
+
+  .material-checkbox-container {
+    cursor: pointer;
+    /* padding: 0 1rem; */
+  }
+
+  .material-checkbox-container > input {
+    position: absolute;
+    transform: scale(0);
+  }
+
+  .material-checkbox-container > .material-checkbox {
+    display: block;
+    width: 21px;
+    height: 21px;
+    border: 2px solid rgba(255, 255, 255, 0.175);
+    transition: all 0.35s;
+  }
+
+  .material-checkbox-container > input:checked ~ .material-checkbox {
+    transform: rotate(45deg) translate(2px, -2px);
+    width: 7.5px;
+    border-color: #1ac0a2;
+    border-top-color: transparent;
+    border-left-color: transparent;
+  }
+
+  .form-control {
+    padding: 0.4rem 0;
   }
 </style>
