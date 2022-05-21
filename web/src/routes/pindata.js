0b1721247e775c0ebb09f412142c1cb48de33800
eugenioclrc <eugenioclrc@gmail.com>
2022-05-20 22:33:56 -0300

    sample storjk upload

--- web/src/routes/pindata.js
+++ web/src/routes/pindata.js
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv';
 import { ethers } from 'ethers';
+import fetch, { FormData, File, fileFrom } from 'node-fetch'
+
+import { Blob } from 'buffer';
 
 dotenv.config();
 
@@ -7,14 +10,37 @@ dotenv.config();
 
 /** @type {import('./index').RequestHandler} */
 export const post = async ({ request, locals }) => {
-	const form = await request.formData();
+	return {
+		body: {
+            form: "TODO"
+		}
+	}
+};
 
 
 
+/** @type {import('./index').RequestHandler} */
+export const get = async ({ request, locals }) => {
+
+    const body = new FormData()
+    const file = new File(['text'], 'abc.txt', { type: 'text/plain' })
+
+
+    body.set('file', file);
+
+    const response = await fetch("https://demo.storj-ipfs.com/api/v0/add", {
+        method: "POST",
+        body,
+    });
+
+    const { Hash } = await response.json();
+
+    // sample url:
+    // https://demo.storj-ipfs.com/ipfs/QmY2T5EfgLn8qWCt8eus6VX1gJuAp1nmUSdmoehgMxznAf
 
 	return {
 		body: {
-            form
+            e: Hash
 		}
 	}
 };
