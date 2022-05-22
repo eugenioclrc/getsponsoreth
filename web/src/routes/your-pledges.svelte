14d2a9c6a06baf6374857ca94b8512cfaa27a7d2
Deivitto <info.dcm62@gmail.com>
2022-05-22 02:17:59 +0200

    new pedge for your pledges

---
+++ web/src/routes/your-pledges.svelte
@@ -0,0 +1,210 @@
+<script>
+  import { onMount } from "svelte";
+  var data = {
+    title: "Pledge reason",
+    description: "Svelte demo app",
+    user_image: "https://avatars0.githubusercontent.com/u/1234?s=460&v=4",
+    name: "John Doe",
+  };
+
+  function clickDoShit() {
+    const formData = new FormData();
+
+    // JavaScript file-like object...
+    var content = '<a id="a"><b id="b">hey!</b></a>'; // the body of the new file...
+    var blob = new Blob([content], { type: "text/xml" });
+
+    formData.append("file", blob);
+
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://demo.storj-ipfs.com/api/v0/add");
+    request.send(formData);
+  }
+
+  const backgroundImages = [
+    "joshua-earle-Hn8N4I4eHA0-unsplash.jpg",
+    "natalie-runnerstrom-SZlgOP7bSnI-unsplash.jpg",
+    "oliver-spicer-NmPNw8w_a24-unsplash.jpg",
+    "xan-griffin-eA2t5EvcxU4-unsplash.jpg",
+  ];
+
+  //function for getting a random string of backgroundImages
+
+  let randomIndex = 0;
+  onMount(() => {
+    randomIndex = Math.floor(Math.random() * backgroundImages.length);
+  });
+
+  $: backgroundImage = backgroundImages[randomIndex];
+</script>
+
+<div
+  class="pt-16 pb-12 min-h-screen"
+  style="background-image: url({backgroundImage});
+background-repeat: no-repeat;
+background-size: cover;
+"
+>
+  <!-- container with 2 responsives columns -->
+  <div class="container mx-auto px-4 ">
+    <div class="logo-container ">
+      <a href="/"
+        ><img
+          class="logo-image"
+          src="./sponsrethgold2.png"
+          alt="sponsor.eth"
+        /></a
+      >
+    </div>
+    <!-- centered image -->
+    <div
+      class="flex flex-col lg:flex-row mt-4"
+      style="column-count: 2; flex-wrap: wrap;"
+    >
+      <div class="lg:w-2/3 mt-2">
+        <!-- title Message -->
+
+        <div class="  flex flex-col ">
+          <!-- title -->
+          <div
+            class="card flex-shrink-0 w-full pledge-card shadow-2xl bg-base-100 flex flex-row items-center  mb-4 mt-4"
+          >
+            <h2 class="p-8 pt-6 pb-6 Messages_Title">Your pledges</h2>
+          </div>
+          <!-- Start of component -->
+          <!-- card that contains lorem ipsum -->
+          <div
+            class="card flex-shrink-0 w-full pledge-card shadow-2xl bg-base-100 flex flex-row items-center "
+          >
+            <!-- avatar -->
+            <div class="avatar  p-8">
+              <div class="w-24 mask mask-circle">
+                <img src="https://api.lorem.space/image/face?hash=53273" />
+              </div>
+            </div>
+
+            <!-- message -->
+            <div class="message pt-8 pb-8 pr-8">
+              <div class="message-content flex flex-row ">
+                <div class="message-header">
+                  <h1 class="text-2xl font-bold pb-2">{data.name}</h1>
+                  <h2 class="text-sm font-bold pb-4">
+                    {data.title}
+                  </h2>
+                </div>
+              </div>
+            </div>
+            <div class="ml-auto pr-8 buttons">
+              <!-- button claim -->
+              <button class="btn btn-primary btn-outline">Edit</button>
+
+              <button class="btn button-charming ">Claim</button>
+            </div>
+          </div>
+
+          <!-- End of component -->
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .custom-card {
+    /* border: 1px solid #ccc; */
+  }
+
+  .pledge-card {
+    max-width: 80%;
+    height: 80%;
+  }
+
+  .payment-card {
+    max-height: 450px;
+  }
+
+  .content-column {
+    margin-top: 3rem;
+  }
+
+  .logo-container {
+    position: relative;
+    top: 1rem;
+    /* left: 1rem; */
+  }
+
+  .avatar-container {
+    padding: 2rem;
+    padding-bottom: 1rem;
+  }
+
+  .Messages_Title {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-left: 2rem;
+  }
+
+  .Pledge_Title {
+    font-size: 36px;
+  }
+
+  .logo-image {
+    border-radius: 35px;
+    max-width: 200px;
+    /* margin-left: auto; */
+    margin-right: auto;
+    transition: ease 0.5s;
+  }
+
+  .logo-image:hover {
+    transform: translateX(10px);
+  }
+
+  .buttons > button {
+    min-width: 150px;
+  }
+
+  .button-charming {
+    height: 3rem;
+    /* border-radius: 0.rem; */
+    padding: 0 2rem;
+    background: linear-gradient(-45deg, #9925ea, #338aff, #9925ea);
+    background-size: 400%;
+    background-position: 90% 0;
+    color: #fff;
+    transition: background 0.8s;
+    border: none;
+  }
+
+  .button-charming:hover {
+    background-position: 185% 50%;
+  }
+
+  @media (max-width: 768px) {
+    .Pledge_Title {
+      font-size: 26px;
+    }
+  }
+
+  @media (max-width: 1000px) {
+    .card {
+      margin-left: auto;
+      margin-right: auto;
+      width: 100%;
+      max-width: 100%;
+    }
+
+    .logo-container {
+      margin-right: auto;
+      margin-left: auto;
+      justify-content: center;
+      text-align: center;
+      display: flex;
+      left: initial;
+    }
+
+    .min-h-screen {
+      min-height: initial;
+    }
+  }
+</style>
