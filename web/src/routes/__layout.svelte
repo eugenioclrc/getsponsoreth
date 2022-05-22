c26e4397b9ae76018c4c3c58dccfdbf428303157
eugenioclrc <eugenioclrc@gmail.com>
2022-05-21 21:56:44 -0300

    merge

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -22,5 +22,6 @@
   <!-- this is for the web3modal -->
   <script type="text/javascript" src="https://unpkg.com/web3modal@1.9.0/dist/index.js"></script>
   <script type="text/javascript" src="https://unpkg.com/@walletconnect/web3-provider@1.2.1/dist/umd/index.min.js"></script>
+  <script type="text/javascript" src="/wallet-sdk-bundle.js"></script>
 </svelte:head>
 <slot />
--- web/src/routes/pledge-view.svelte
+++ web/src/routes/pledge-view.svelte
@@ -1,7 +1,6 @@
 <script>
   import { onMount } from "svelte";
 
-  import { onMount } from "svelte";
 
 
   import {
